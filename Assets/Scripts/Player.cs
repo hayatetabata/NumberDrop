61b6875c7ce6230d295f66fae2de09dc4254a9c3
田畑颯 <chinarra.tabata@gmail.com>
2018-11-02 12:33:46 +0900

    Create player script

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public int power;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Camera.main.transform.position.y > transform.position.y) {
+            GetComponent<Rigidbody2D>().gravityScale = 0;
+        }
+    }
+}
