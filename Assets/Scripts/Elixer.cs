45f2a8c3a1494b73d28c483f0e914c88fb183c8a
田畑颯 <chinarra.tabata@gmail.com>
2018-11-03 21:57:04 +0900

    Extend base object class

---
+++ Assets/Scripts/Elixer.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Elixer : BaseObject {
+
+	// Update is called once per frame
+    void Update () {
+        Vector2 dist = new Vector2(0f, +1f);
+        Move(dist, __speed);	
+	}
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,28 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour {
+public class Player : BaseObject {
 
-    public int power;
-    public float speed;
+    public int __power;
 
-	// Use this for initialization
-	void Start () {
-
-	}
-	
 	// Update is called once per frame
 	void Update () {
         if (Camera.main.transform.position.y < transform.position.y) {
-            Move();
+            Vector2 dist = new Vector2(0f, -1f);
+            Move(dist, __speed);
         }
-    }
-
-    void Move()
-    {
-        Vector2 pos = transform.position;
-        Vector2 dist = new Vector2(0f, -1f);
-        Vector2 newPos = pos + dist.normalized * speed;
-        transform.position = newPos;
     }
 }
