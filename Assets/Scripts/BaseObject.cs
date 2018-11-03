4b2ea716d28c89ee31b7744e918124a18d449272
田畑颯 <chinarra.tabata@gmail.com>
2018-11-03 21:56:53 +0900

    Create base object

---
+++ Assets/Scripts/BaseObject.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class BaseObject : MonoBehaviour {
+
+    public float __speed;
+
+    protected void Move(Vector2 dist, float speed)
+    {
+        Vector2 pos = transform.position;
+        Vector2 newPos = pos + dist.normalized * speed;
+        transform.position = newPos;
+    }
+}
