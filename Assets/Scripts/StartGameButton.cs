e9a1393901466160e053bc0bba347631508b00ab
田畑颯 <chinarra.tabata@gmail.com>
2018-11-04 17:46:49 +0900

    Create start game scene

---
+++ Assets/Scripts/StartGameButton.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartGameButton : MonoBehaviour {
+
+    public void OnClick()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+}
